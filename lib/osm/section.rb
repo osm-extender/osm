9e26a0ee02ede4432122e48a6f400fef4caea4bf
Robert Gauld <robert@robertgauld.co.uk>
2012-08-30 20:29:09 +0100

    Remove forgotten debug code.

--- lib/osm/section.rb
+++ lib/osm/section.rb
@@ -32,7 +32,6 @@ module Osm
     # Initialize a new Section
     # @param [Hash] attributes the hash of attributes (see attributes for descriptions, use Symbol of attribute name as the key)
     def initialize(attributes={})
-puts ":num_scouts = #{attributes[:num_scouts].inspect}"
       raise ArgumentError, ':id must be nil or a Fixnum > 0' unless attributes[:id].nil? || (attributes[:id].is_a?(Fixnum) && attributes[:id] > 0)
       raise ArgumentError, ':section_name must be nil or a String' unless attributes[:section_name].nil? || attributes[:section_name].is_a?(String)
       raise ArgumentError, ':num_scouts must be nil or a Fixnum >= 0' unless attributes[:num_scouts].nil? || (attributes[:num_scouts].is_a?(Fixnum) && attributes[:num_scouts] >= 0)
