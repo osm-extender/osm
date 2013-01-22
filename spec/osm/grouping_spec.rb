a5b6c14cf781b4730692bf8226de8973d09659af
Robert Gauld <robert@robertgauld.co.uk>
2013-01-22 13:10:26 +0000

    Fix exception when OSM returns empty string when requesting groupings for a section

--- spec/osm/grouping_spec.rb
+++ spec/osm/grouping_spec.rb
@@ -3,24 +3,34 @@ require 'spec_helper'
 
 describe "Grouping" do
 
-  it "Create" do
-    body = {'patrols' => [{
-      'patrolid' => 1,
-      'name' => 'Patrol Name',
-      'active' => 1,
-      'points' => '3',
-    }]}
-    FakeWeb.register_uri(:post, "https://www.onlinescoutmanager.co.uk/users.php?action=getPatrols&sectionid=2", :body => body.to_json)
+  describe "Using the API" do
 
-    patrols = Osm::Grouping.get_for_section(@api, 2)
-    patrols.size.should == 1
-    patrol = patrols[0]
-    patrol.id.should == 1
-    patrol.section_id.should == 2
-    patrol.name.should == 'Patrol Name'
-    patrol.active.should == true
-    patrol.points.should == 3
-    patrol.valid?.should be_true
+    it "Create" do
+      body = {'patrols' => [{
+        'patrolid' => 1,
+        'name' => 'Patrol Name',
+        'active' => 1,
+        'points' => '3',
+      }]}
+      FakeWeb.register_uri(:post, "https://www.onlinescoutmanager.co.uk/users.php?action=getPatrols&sectionid=2", :body => body.to_json)
+
+      patrols = Osm::Grouping.get_for_section(@api, 2)
+      patrols.size.should == 1
+      patrol = patrols[0]
+      patrol.id.should == 1
+      patrol.section_id.should == 2
+      patrol.name.should == 'Patrol Name'
+      patrol.active.should == true
+      patrol.points.should == 3
+      patrol.valid?.should be_true
+    end
+
+    it "Handles no data" do
+      FakeWeb.register_uri(:post, "https://www.onlinescoutmanager.co.uk/users.php?action=getPatrols&sectionid=2", :body => '')
+      patrols = Osm::Grouping.get_for_section(@api, 2)
+      patrols.size.should == 0
+    end
+
   end
 
 end
