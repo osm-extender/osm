14b75dd12f8d5a034d03d655fefd7d8a3d9de2d8
Robert Gauld <robert@robertgauld.co.uk>
2015-05-28 12:23:32 +0100

    Fix validity_validator_spec failing when run as part of test suite

--- spec/array_of_validator_spec.rb
+++ spec/array_of_validator_spec.rb
@@ -2,99 +2,105 @@
 require 'spec_helper'
 
 
-class FixnumTestModel < Osm::Model
-  attribute :array
-  validates :array, :array_of => {:item_type => Fixnum}
-end
+module ArrayOfValidatorSpec
 
-class TestItem
-  def initialize(attrs)
-    @valid = !!attrs[:valid]
-  end
-  def valid?
-    @valid
-  end
-end
-
-class ValidTestModel < Osm::Model
-  attribute :array
-  validates :array, :array_of => {:item_type => TestItem, :item_valid=>true}
-end
-
-class InvalidTestModel < Osm::Model
-  attribute :array
-  validates :array, :array_of => {:item_type => TestItem, :item_valid=>false}
-end
-
-class NovalidTestModel < Osm::Model
-  attribute :array
-  validates :array, :array_of => {:item_type => TestItem}
-end
-
-
-describe "Array of validator" do
-
-  it "Allows an empty array" do
-    i = FixnumTestModel.new(array: [])
-    i.valid?.should == true
-    i.errors.count.should == 0
+  class FixnumTestModel < Osm::Model
+    attribute :array
+    validates :array, :array_of => {:item_type => Fixnum}
   end
 
-  describe ":item_type option" do
+  class TestItem
+    def initialize(attrs)
+      @valid = !!attrs[:valid]
+    end
+    def valid?
+      @valid
+    end
+  end
 
-    it "Allows arrays of the right type" do
-      i = FixnumTestModel.new(array: [1, 2, 3])
+  class ValidTestModel < Osm::Model
+    attribute :array
+    validates :array, :array_of => {:item_type => TestItem, :item_valid=>true}
+  end
+
+  class InvalidTestModel < Osm::Model
+    attribute :array
+    validates :array, :array_of => {:item_type => TestItem, :item_valid=>false}
+  end
+
+  class NovalidTestModel < Osm::Model
+    attribute :array
+    validates :array, :array_of => {:item_type => TestItem}
+  end
+
+
+ describe "Array of validator" do
+
+    it "Allows an empty array" do
+      i = FixnumTestModel.new(array: [])
       i.valid?.should == true
       i.errors.count.should == 0
     end
 
-    it "Forbids arrays containing >= 1 incorrect type" do
-      i = FixnumTestModel.new(array: [1, '2', 3])
-      i.valid?.should == false
-      i.errors.count.should == 1
-      i.errors.messages.should == {:array=>["items in the Array must be a Fixnum"]}
-    end
+    describe ":item_type option" do
 
-  end
-
-  describe ":valid option" do
-
-    it "Allows (in)valid items unless valid option is passed" do
-      i = NovalidTestModel.new(array: [TestItem.new(valid: false), TestItem.new(valid: true)])
-      i.valid?.should == true
-      i.errors.count.should == 0
-    end
-
-    describe "Valid option is false" do
-
-      it "Contains all invalid items" do
-        i = InvalidTestModel.new(array: [TestItem.new(valid: false)])
+      it "Allows arrays of the right type" do
+        i = FixnumTestModel.new(array: [1, 2, 3])
         i.valid?.should == true
         i.errors.count.should == 0
       end
 
-      it "Contains a valid item" do
-        i = InvalidTestModel.new(array: [TestItem.new(valid: true)])
+      it "Forbids arrays containing >= 1 incorrect type" do
+        i = FixnumTestModel.new(array: [1, '2', 3])
         i.valid?.should == false
         i.errors.count.should == 1
-        i.errors.messages.should == {:array => ['contains a valid item']}
+        i.errors.messages.should == {:array=>["items in the Array must be a Fixnum"]}
       end
 
     end
 
-    describe "Valid option is true" do
+    describe ":valid option" do
 
-      it "Contains all valid items" do
-        i = ValidTestModel.new(array: [TestItem.new(valid: true)])
+      it "Allows (in)valid items unless valid option is passed" do
+        i = NovalidTestModel.new(array: [TestItem.new(valid: false), TestItem.new(valid: true)])
         i.valid?.should == true
         i.errors.count.should == 0
       end
 
-      it "Contains an invalid item" do
-        i = ValidTestModel.new(array: [TestItem.new(valid: false)])
-        i.valid?.should == false
-        i.errors.count.should == 1
-        i.errors.messages.should == {:array => ['contains an invalid item']}      end
+      describe "Valid option is false" do
+
+        it "Contains all invalid items" do
+          i = InvalidTestModel.new(array: [TestItem.new(valid: false)])
+          i.valid?.should == true
+          i.errors.count.should == 0
+        end
+
+        it "Contains a valid item" do
+          i = InvalidTestModel.new(array: [TestItem.new(valid: true)])
+          i.valid?.should == false
+          i.errors.count.should == 1
+          i.errors.messages.should == {:array => ['contains a valid item']}
+        end
+
+      end
+
+      describe "Valid option is true" do
+
+        it "Contains all valid items" do
+          i = ValidTestModel.new(array: [TestItem.new(valid: true)])
+          i.valid?.should == true
+          i.errors.count.should == 0
+        end
+
+        it "Contains an invalid item" do
+          i = ValidTestModel.new(array: [TestItem.new(valid: false)])
+          i.valid?.should == false
+          i.errors.count.should == 1
+          i.errors.messages.should == {:array => ['contains an invalid item']}
+        end
+
+      end
+
     end
 
   end
--- spec/validity_validator_spec.rb
+++ spec/validity_validator_spec.rb
@@ -1,56 +1,59 @@
 # encoding: utf-8
 require 'spec_helper'
 
-
-class TestItem
-  include ActiveAttr::Model
-  attribute :validity
-  validates_inclusion_of :validity, :in => [true]
-end
-
-class TestModel
-  include ActiveAttr::Model
-  attribute :item
-  validates :item, :validity => true
-end
-class TestModelAllowNil
-  include ActiveAttr::Model
-  attribute :item
-  validates :item, :validity => {allow_nil: true}
-end
-class TestModelDisallowNil
-  include ActiveAttr::Model
-  attribute :item
-  validates :item, :validity => {allow_nil: false}
-end
-
-describe "validity validator" do
-
-  it "Item is valid" do
-    model = TestModel.new(item: TestItem.new(validity: true))
-    model.valid?.should == true
-    model.errors.count.should == 0
+module ValidityValidatorSpec
+  class TestItem
+    include ActiveAttr::Model
+    attribute :validity
+    validates_inclusion_of :validity, :in => [true]
   end
 
-  it "Item is invalid" do
-    model = TestModel.new(item: TestItem.new(validity: false))
-    model.valid?.should == false
-    model.errors.count.should == 2
-    model.errors.messages.should == {:item => ['must be valid', 'validity attribute is invalid: is not included in the list']}
+  class TestModel
+    include ActiveAttr::Model
+    attribute :item
+    validates :item, :validity => true
+  end
+  class TestModelAllowNil
+    include ActiveAttr::Model
+    attribute :item
+    validates :item, :validity => {allow_nil: true}
+  end
+  class TestModelDisallowNil
+    include ActiveAttr::Model
+    attribute :item
+    validates :item, :validity => {allow_nil: false}
   end
 
-  describe "Allow nil" do
 
-    it "Is true" do
-      TestModelAllowNil.new(item: TestItem.new(validity: true)).valid?.should == true
-      TestModelAllowNil.new(item: TestItem.new(validity: false)).valid?.should == false
-      TestModelAllowNil.new(item: nil).valid?.should == true
+  describe "validity validator" do
+
+    it "Item is valid" do
+      model = TestModel.new(item: TestItem.new(validity: true))
+      model.valid?.should == true
+      model.errors.count.should == 0
     end
 
-    it "Is false" do
-      TestModelDisallowNil.new(item: TestItem.new(validity: true)).valid?.should == true
-      TestModelDisallowNil.new(item: TestItem.new(validity: false)).valid?.should == false
-      TestModelDisallowNil.new(item: nil).valid?.should == false
+    it "Item is invalid" do
+      model = TestModel.new(item: TestItem.new(validity: false))
+      model.valid?.should == false
+      model.errors.count.should == 2
+      model.errors.messages.should == {:item => ['must be valid', 'validity attribute is invalid: is not included in the list']}
+    end
+
+    describe "Allow nil" do
+
+      it "Is true" do
+        TestModelAllowNil.new(item: TestItem.new(validity: true)).valid?.should == true
+        TestModelAllowNil.new(item: TestItem.new(validity: false)).valid?.should == false
+        TestModelAllowNil.new(item: nil).valid?.should == true
+      end
+
+      it "Is false" do
+        TestModelDisallowNil.new(item: TestItem.new(validity: true)).valid?.should == true
+        TestModelDisallowNil.new(item: TestItem.new(validity: false)).valid?.should == false
+        TestModelDisallowNil.new(item: nil).valid?.should == false
+      end
+
     end
 
   end
